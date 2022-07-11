894e5040250dd55876f3067a46ce33e61a1e6c58
menachem554 <mendyrivkin@gmail.com>
2022-07-11 15:36:25 +0300

    m

---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+import * as env from 'env-var';
+import './dotenv';
+
+const config = {
+    service: {
+        port: env.get('PORT').required().asPortNumber(),
+    },
+    mongo: {
+        uri: env.get('MONGO_URI').required().asUrlString(),
+        featureCollectionName: env.get('MONGO_FEATURE_COLLECTION_NAME').required().asString(),
+    },
+    rabbit: {
+        uri: env.get('RABBIT_URI').required().asUrlString(),
+        retryOptions: {
+            minTimeout: env.get('RABBIT_RETRY_MIN_TIMEOUT').default(1000).asIntPositive(),
+            retries: env.get('RABBIT_RETRY_RETRIES').default(10).asIntPositive(),
+            factor: env.get('RABBIT_RETRY_FACTOR').default(1.8).asFloatPositive(),
+        },
+    },
+};
+
+export default config;
